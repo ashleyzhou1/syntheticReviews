d01bc147ec3db749c47328c84ffdcb6727ef5cd5
ashleyzhou1 <69983731+ashleyzhou1@users.noreply.github.com>
2025-01-03 20:29:58 -0800

    Update reviews_grammar_check.py

--- reviews_grammar_check.py
+++ reviews_grammar_check.py
@@ -8,22 +8,21 @@ nltk.download("vader_lexicon")
 tool = language_tool_python.LanguageTool('en-US')
 reviews = [];
 corrected_reviews = [];
-#sums of sentiment scores
 
+#first use LanguageTool to edit synthetic reviews for grammar
 def grammar_check(title_text):
     matches = tool.check(title_text)
     if len(matches)!=0:
         print(matches)
         print("\n")
-        with open('/Users/ashleyzhou/Desktop/matches.txt', 'a') as text_file:
+        with open(<file name>, 'a') as text_file:
             for match in matches:
              text_file.write(match)
     corrected_title_text = language_tool_python.utils.correct(title_text, matches) 
     corrected_reviews.append(corrected_title_text)    
-'''
-'''
-#function opens text file and adds difference of each score between title and text
-#for each review. 
+
+#function opens text file and adds difference of each score between title and text 
+#using Vader Sentiment Analysis
 def open_file(file_name):
     neg_sum = 0
     neu_sum = 0
@@ -48,33 +47,8 @@ def open_file(file_name):
             compound_sum+=compound_diff
         print(neg_sum, neu_sum, pos_sum, compound_sum)
 
+#function returning Vader Sentiment Analysis scores
 def get_positivity_score(statement):
     analyzer = SentimentIntensityAnalyzer()
     score = analyzer.polarity_scores(statement)
     return score['neg'], score['neu'], score['pos'], score['compound']
-
-open_file('/Users/ashleyzhou/Desktop/amazon_reviews/All_Beauty.jsonl')
-
-for i in range(1,11):
-    file_name = '/Users/ashleyzhou/Desktop/syntheticData/100syndata'+str(i)+'.jsonl'
-    
-    #the numbers printed are the average difference of scores in each file 
-    #neg, neu, pos, compound
-print("\n\n")
-for i in range(1,11):
-    file_name = '/Users/ashleyzhou/Desktop/syntheticData/500syndata'+str(i)+'.jsonl'
-    
-print("\n\n")
-for i in range(1,11):
-    file_name = '/Users/ashleyzhou/Desktop/syntheticData/1000syndata'+str(i)+'.jsonl'
-   
-'''
-with open('/Users/ashleyzhou/Desktop/corrected_reviews.txt', 'w') as text_file:
-    for corrected_review in corrected_reviews:
-        text_file.write(corrected_review+"\n\n")
-
-with open('/Users/ashleyzhou/Desktop/orig_reviews.txt', 'w') as text_file:
-    for review in reviews:
-        text_file.write(review+"\n\n")
-'''
-   
